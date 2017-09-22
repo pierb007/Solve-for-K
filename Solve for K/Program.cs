78aa01a8eb78a82e1fa932c002da15e57770c0be
Pier <james.pier@outlook.com>
2017-09-22 07:53:33 -0400

    Add project files.

---
+++ Solve for K/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve_for_K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string to be checked for k-periodicity.");
+            Char[] input = Console.ReadLine().ToCharArray();
+            Console.WriteLine("K={0}", ((from c in input select c).Distinct().Count()).ToString());
+            Console.ReadKey();
+        }
+    }
+}
